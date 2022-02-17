eb905b08617582f4ade656edfadff743f363cf0d
Carlos E <elizaldc@protonmail.com>
2022-02-17 13:55:52 -0600

    Enabled devtool in webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do List',
+    }),
+  ],
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
