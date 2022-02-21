9fdbc075e7b53c94682ccab45e381d93037254c7
Carlos E <elizaldc@protonmail.com>
2022-02-21 09:37:40 -0600

    Added heading for page through DOMconstructor and made style changes to Heading.

--- src/DOMconstructor.js
+++ src/DOMconstructor.js
@@ -5,9 +5,11 @@ const createPage = (()=> {
         const navBar = document.createElement('div');
         navBar.classList.add('navbar');
 
-        const navBarText = document.createElement('h1');
+        const navBarText = document.createElement('div');
         navBarText.classList.add('navbar-text');
+        const content = addNavBarText();
 
+        navBarText.appendChild(content);
         navBar.appendChild(navBarText);
         body.appendChild(navBar);
     }
@@ -26,6 +28,12 @@ const createPage = (()=> {
 
         body.appendChild(inbox);
     }
+
+    function addNavBarText() {
+        const navbarText = document.createElement('h1');
+        navbarText.textContent = "To-Do List";
+        return navbarText;
+    };
 
     return { createNavBar, createSideBar, createInbox };
 })();
